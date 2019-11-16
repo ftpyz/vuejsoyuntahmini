c75352e226c7c234302af6abe9fde4da4a1ca1a3
Fuat <fuat@fuatpoyraz.com>
2019-11-16 14:41:32 +0300

    vuetify kuruldu

---
+++ src/components/Oyun.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-if="anaDurum">
+      1 ile 10 arasındaki aklımdan tuttuğum sayıyı tahmin etmeye ne dersin.
+      <br />
+      <input type="number" v-model="KullanicininTahmini" />
+      <br />
+      Tahmin Sayisi:{{ KacinciTahmin }}
+      <button @click="TahminEt">Tahmin Yap</button>
+      <button @click="YenidenBasla">Yeniden Başla</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Oyun",
+  props: ["anaDurum"],
+  data: () => {
+    return {
+      KullanicininTahmini: "",
+      AklimdakiSayi: Math.floor(Math.random() * 10) + 1,
+      KacinciTahmin: 0
+    };
+  },
+  methods: {
+    YenidenBasla() {
+      this.$emit("oyunDurumu", false);
+    },
+    TahminEt() {
+      this.KacinciTahmin++;
+      if (parseInt(this.KullanicininTahmini) == this.AklimdakiSayi) {
+        alert("Tebrikler!!");
+      } else {
+        alert("Çok yaklaştın tekrar denemelisin");
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/OyunBaslangic.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-if="!AdSoyadBaslangic">
+      <p>Merhaba; Sayı tahmin oyunumuza hoş geldiniz. Lütfen başlamak için ad ve soyadınızı giriniz</p>
+      <input type="text" v-model="AdSoyad" />
+      <button v-if="AdSoyad.length >=3" @click="OyunaBasla">Oyuna Başla</button>
+    </div>
+    <div
+      v-else
+    >Merhaba; {{AdSoyadBaslangic}} oyunumuza hoş geldiniz. Sayı tahmininde bulunmaya hazırmısın!</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "OyunBaslangici",
+  data: () => {
+    return {
+      AdSoyad: "",
+      AdSoyadBaslangic: null
+    };
+  },
+  methods: {
+    OyunaBasla() {
+      this.AdSoyadBaslangic = this.AdSoyad;
+      this.$emit("oyunDurumu", true);
+    }
+  }
+};
+</script>
